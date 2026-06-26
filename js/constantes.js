34f2bb9e51234c0bbcc8d3bdc08ced8e44993290
SebasRincon10 <sebasrincon.10@hotmail.com>
2026-06-26 14:56:03 -0500

    Realizado

---
+++ js/constantes.js
@@ -0,0 +1,8 @@
+export const Pi = Math.PI;
+
+export let usuario = "Sebas";
+let password = "qazwsx";
+
+export default function saludar() {
+    console.log("Hola Modulos +ES6")
+}
---
+++ js/modulos.js
@@ -0,0 +1,6 @@
+import saludar, {Pi,usuario} from "./constantes.js";
+import { aritmetica } from "./aritmetica.js";
+console.log("Archivo modulos.js");
+console.log(Pi,usuario);
+console.log(aritmetica.sumar(3,4));
+//saludar();
